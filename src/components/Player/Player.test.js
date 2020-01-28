bcb8db019fb7a7eadf4f4c328bb609d61eae2d3f
Maciej Sznurawa <msznurawa@gmail.com>
2020-01-28 09:55:00 +0100

    Add Player buttons tests

--- src/components/Player/Player.test.js
+++ src/components/Player/Player.test.js
@@ -23,3 +23,34 @@ it('renders correct score', () => {
 
   expect(playerScoreRendered).toEqual(playerScorePassed);
 });
+
+it('should call onPlayerScoreChange with 1 when plus button is clicked', () => {
+  // mockowanie - zasymulowanie wykonania callbacka przekazanego w props, tutaj używamy jest.fn()
+  const mockedOnPlayerScoreChange = jest.fn();
+  const playerComponent = shallow(<Player onPlayerScoreChange={mockedOnPlayerScoreChange} />);
+
+  // symulowanie kliknięcia w button
+  const plusButton = playerComponent.find('.Player__button').first();
+
+  plusButton.simulate('click');
+
+  // expect(mockedOnPlayerScoreChange).toBeCalled();
+  expect(mockedOnPlayerScoreChange).toBeCalledWith(1);
+
+});
+
+it('should call onPlayerScoreChange with -1 when minus button is clicked', () => {
+  // mockowanie - zasymulowanie wykonania callbacka przekazanego w props, tutaj używamy jest.fn()
+  const mockedOnPlayerScoreChange = jest.fn();
+  const playerComponent = shallow(<Player onPlayerScoreChange={mockedOnPlayerScoreChange} />);
+
+  // symulowanie kliknięcia w button; at() nnumeracja od 0 jak w tablicach
+  const plusButton = playerComponent.find('.Player__button').at(1);
+
+  plusButton.simulate('click');
+
+  // expect(mockedOnPlayerScoreChange).toBeCalled();
+  expect(mockedOnPlayerScoreChange).toBeCalledWith(-1);
+
+});
+
